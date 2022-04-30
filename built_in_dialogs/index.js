1a7ca803f87724843de8bee4ee4d61c96eb95834
Adebayo  Micheal <mcgamma04@yahoo.com>
2022-04-30 15:56:03 +0100

    variable modified and built dialog like alert and confirm added

---
+++ built_in_dialogs/index.js
@@ -0,0 +1,15 @@
+//alert : 1
+//alert("This is cool");
+
+//confirm dialog: 2
+
+//confirm("Do you really want to delete?");
+
+//Prompt : 3
+
+let firstNumber = parseInt(prompt("Enter first number: "));
+let secondNumber =  parseInt(prompt("Enter second number"));
+
+let sum =  firstNumber + secondNumber;
+document.write(`<h2>sum= ${sum}</h1>`);
+console.log("sum =  "+sum);
