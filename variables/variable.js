b94d642ed7e923f49b4829dd3c225c40f342b40f
Adebayo  Micheal <mcgamma04@yahoo.com>
2022-04-30 15:39:32 +0100

    Done with variable declaration

--- variables/variable.js
+++ variables/variable.js
@@ -2,5 +2,28 @@
 // variable is a placeholder that hold an information in computer
 // memory. It is container that contains an in inormation
 
+//There are three ways to declare variable in JavaScript
+
+//1. var, 2.  let 3. const
+
+//var is for global variable declaration
+var a = 7;
+var b = 5;
+var sum = a + b;
+//console.log("sum = "+sum);
+//let is use for local variable declaration
+let num1, num2, total;
+num1 = 2;
+num2 = 10;
+num1 = 90;
+total = num1+ num2;
+//console.log(`total= ${total}`);
+//concatenation: is the joining two or more strings. To concatenate in js simply use + sign
+const first = 400;
+const second =  300;
+//first =  230; error becuase constant variable cannot be reassigned
+let result = first + second;
+console.log(`Result =  ${result}`);
+
 
 
