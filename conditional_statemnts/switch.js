5e038b559fa34efd098be443dd355c90f774f068
Adebayo  Micheal <mcgamma04@yahoo.com>
2022-05-28 09:53:50 +0100

    Modified switch

--- conditional_statemnts/switch.js
+++ conditional_statemnts/switch.js
@@ -1,11 +1,17 @@
 
-let option = parseInt(prompt("1: Add\n2:substract\n3:Multiply\n4:Divide"));
+let option = parseInt(prompt("1: Add\n2:substract\n3:Multiply\n4:Divide\n5Exit"));
 switch(option){
     case 1:
         console.log("addition function will called");
         break;
      case 2:
         console.log("subatract function will called");
+        break;
+    case 3:
+        console.log("Multply function will called");
+        break;
+    case 5  :
+        console.log("Bye bye");
         break;
     default:
         console.log("wrong input");
