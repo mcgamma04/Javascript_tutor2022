d67930a32955c598e90fa4f67b92394282c3d1c0
Adebayo  Micheal <mcgamma04@yahoo.com>
2022-06-01 00:49:26 +0100

    smallest algorithm added

---
+++ arrays/secondsmallest.js
@@ -0,0 +1,15 @@
+//return the second smallest and second largest in the given array
+
+function secondlest(arr){
+//sort the array
+arr = arr.sort();
+//remove duplicate
+arr = [... new Set(arr)];
+arr.pop();
+arr.shift();
+let max = arr.pop();
+let min = arr.shift();
+return [min, max];
+}
+const array = [2,5,7,2,8,9];
+console.log(secondlest(array));
