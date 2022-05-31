11226c4f0191429fa461bbebcc5535c4c3aeabbf
Adebayo  Micheal <mcgamma04@yahoo.com>
2022-05-31 12:14:33 +0100

    include and array conversion  added

---
+++ arrays/arraycont.js
@@ -0,0 +1,17 @@
+const arr =  [2,5,7,8,5];
+//includes
+
+if(arr.includes(80)){
+    console.log("yes its");
+}else{
+console.log("it is not");
+}
+//indexof
+let res = arr.indexOf(2);
+console.log(res);
+
+//conveer array to string
+let newarr = arr.join('');
+console.log(typeof newarr);
+
+
